8890eb6f9ffe5ec21b0533e86f0b388e8ee32b26
Volkov Sergey <s.vokov@centrvd.ru>
2023-12-05 17:54:10 +0700

    Тестирование функции интеграции.

--- CentrVD.Integration/CentrVD.Integration.Server/ModuleServerFunctions.cs
+++ CentrVD.Integration/CentrVD.Integration.Server/ModuleServerFunctions.cs
@@ -9,5 +9,24 @@ namespace CentrVD.Integration.Server
   public class ModuleFunctions
   {
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name">Тема совещания.</param>
+    /// <param name="datetime">Дата совещания со временем.</param>
+    /// <param name="duration">Длительность совещания.</param>
+    /// <param name="location">Место совещания.</param>
+    [Public(WebApiRequestType = RequestType.Post)]
+    public void CreateMeeting(string name, string datetime, double duration, string location)
+    {
+      var meeting = Sungero.Meetings.Meetings.Create();
+      meeting.Name = name;
+      meeting.Duration = duration;
+      DateTime dt; 
+      meeting.DateTime = Calendar.TryParseDateTime(datetime, out dt) ? DateTime.Parse(datetime) : Calendar.Now;
+      meeting.Location = location;
+      meeting.Save();
+    }
+
   }
 }
